2f769851cb7a5b4ab2ca06e17b1cadc4bf2c55e3
Adeel <adeelbm@outlook.com>
2020-05-28 16:14:36 +0300

    Update YamlDotNet and use LibYamlEventStream

--- docker/dotnet/utils/dotnet-yamldotnet.sh
+++ docker/dotnet/utils/dotnet-yamldotnet.sh
@@ -8,9 +8,9 @@ touch "$HOME"/.dotnet/"$(dotnet --version)".dotnetFirstUseSentinel
 cp -r /buildutils /tmp/buildutils
 build () {
     dotnet publish /tmp/buildutils/"$1" \
-        --nologo                    \
-        --output /build/bin         \
-        --configuration Release     \
+        --nologo                        \
+        --output /build/bin             \
+        --configuration Release         \
         --runtime linux-musl-x64
 }
 
