aa45ad24f2ef7b2dbffc724217ca483968d15c27
Adeel Mujahid <adeelbm@outlook.com>
2020-08-01 21:44:01 +0300

    Update YamlDotNet and use LibYamlEventStream (#5)
    * Update YamlDotNet and use LibYamlEventStream
    
    * Use DOTNET_NOLOGO for FTUE suppression
    This was added in latest SDK, refreshed 3.1 tag

--- docker/dotnet/alpine-builder.dockerfile
+++ docker/dotnet/alpine-builder.dockerfile
@@ -1,3 +1,4 @@
 FROM mcr.microsoft.com/dotnet/core/sdk:3.1-alpine
 
+ENV DOTNET_NOLOGO 1
 ENV DOTNET_CLI_TELEMETRY_OPTOUT 1
--- docker/dotnet/utils/dotnet-yamldotnet.sh
+++ docker/dotnet/utils/dotnet-yamldotnet.sh
@@ -2,15 +2,12 @@
 
 set -x
 
-# to suppress "Welcome to .NET.." FTUE message
-touch "$HOME"/.dotnet/"$(dotnet --version)".dotnetFirstUseSentinel
-
 cp -r /buildutils /tmp/buildutils
 build () {
     dotnet publish /tmp/buildutils/"$1" \
-        --nologo                    \
-        --output /build/bin         \
-        --configuration Release     \
+        --nologo                        \
+        --output /build/bin             \
+        --configuration Release         \
         --runtime linux-musl-x64
 }
 
