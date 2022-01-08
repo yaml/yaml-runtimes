99c736f899905f66e540829507670cce21f62a9e
Andrey Somov <public.somov@gmail.com>
2022-01-08 13:39:54 +0400

    Issue 22: reformat Java sources

--- docker/java/utils/java/src/main/java/org/yaml/editor/Snake2Yaml.java
+++ docker/java/utils/java/src/main/java/org/yaml/editor/Snake2Yaml.java
@@ -1,56 +1,57 @@
 package org.yaml.editor;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.Writer;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.emitter.Emitter;
 import org.yaml.snakeyaml.events.Event;
 import org.yaml.snakeyaml.reader.UnicodeReader;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.io.Writer;
-
 public class Snake2Yaml {
-    /**
-     * Convert a YAML character stream into events and then emit events back to YAML.
-     *
-     * @param in  Stream to read YAML from
-     * @param out Stream to write YAML to
-     */
-    void yamlToYaml(final InputStream in, final PrintStream out) throws IOException {
-        Yaml input = new Yaml();
-        DumperOptions options = new DumperOptions();
-        Emitter emitter = new Emitter(new Out(out), options);
-        for (Event event : input.parse(new UnicodeReader(in))) {
-            emitter.emit(event);
-        }
+
+  /**
+   * Convert a YAML character stream into events and then emit events back to YAML.
+   *
+   * @param in  Stream to read YAML from
+   * @param out Stream to write YAML to
+   */
+  void yamlToYaml(final InputStream in, final PrintStream out) throws IOException {
+    Yaml input = new Yaml();
+    DumperOptions options = new DumperOptions();
+    Emitter emitter = new Emitter(new Out(out), options);
+    for (Event event : input.parse(new UnicodeReader(in))) {
+      emitter.emit(event);
+    }
+  }
+
+  public static void main(final String[] args) throws IOException {
+    new Snake2Yaml().yamlToYaml(System.in, System.out);
+  }
+
+  private class Out extends Writer {
+
+    private final PrintStream out;
+
+    public Out(PrintStream out) {
+      this.out = out;
     }
 
-    public static void main(final String[] args) throws IOException {
-        new Snake2Yaml().yamlToYaml(System.in, System.out);
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+      for (int i = off; i < len; i++) {
+        out.write(cbuf[i]);
+      }
     }
 
-    private class Out extends Writer {
-        private PrintStream out;
-
-        public Out(PrintStream out) {
-            this.out = out;
-        }
-
-        @Override
-        public void write(char[] cbuf, int off, int len) throws IOException {
-            for (int i = off; i < len; i++) {
-                out.write(cbuf[i]);
-            }
-        }
-
-        @Override
-        public void flush() throws IOException {
-        }
-
-        @Override
-        public void close() throws IOException {
-        }
+    @Override
+    public void flush() throws IOException {
     }
+
+    @Override
+    public void close() throws IOException {
+    }
+  }
 }
--- docker/java/utils/java/src/test/java/org/yaml/editor/Snake2YamlTest.java
+++ docker/java/utils/java/src/test/java/org/yaml/editor/Snake2YamlTest.java
@@ -1,33 +1,33 @@
 package org.yaml.editor;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class Snake2YamlTest {
 
-    private void checkConversion(final String input, final String expected) throws IOException {
-        ByteArrayOutputStream uu = new ByteArrayOutputStream();
-        final PrintStream sw = new PrintStream(uu);
-        new Snake2Yaml().yamlToYaml(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)), sw);
-        assertEquals(expected, uu.toString());
-    }
+  private void checkConversion(final String input, final String expected) throws IOException {
+    ByteArrayOutputStream uu = new ByteArrayOutputStream();
+    final PrintStream sw = new PrintStream(uu);
+    new Snake2Yaml().yamlToYaml(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)),
+        sw);
+    assertEquals(expected, uu.toString());
+  }
 
-    @Test
-    public void simpleIntArray() throws IOException {
-        checkConversion("- 1\n- 2\n- 3", "- 1\n- 2\n- 3\n");
-    }
+  @Test
+  public void simpleIntArray() throws IOException {
+    checkConversion("- 1\n- 2\n- 3", "- 1\n- 2\n- 3\n");
+  }
 
-    @Test
-    public void oneDocument() throws IOException {
-        checkConversion("---\n" +
-                "foo: bar\n", "---\n" +
-                "foo: bar\n");
-    }
+  @Test
+  public void oneDocument() throws IOException {
+    checkConversion("---\n" +
+        "foo: bar\n", "---\n" +
+        "foo: bar\n");
+  }
 }
