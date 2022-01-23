7b886cc28f8985ec03e7fcd834f73c96e5206084
Joao Paulo Magalhaes <dev@jpmag.me>
2022-01-23 16:57:29 +0000

    rapidyaml: add dependency to static, tidy the build script

--- docker/static/utils/cpp-rapidyaml-build.sh
+++ docker/static/utils/cpp-rapidyaml-build.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
 set -x
-set -e
 cd /tmp
 
 # the tool is not yet in any release, and requires building.
 git clone --recursive https://github.com/biojppm/rapidyaml
-cd rapidyaml
 
 cmake --version
-g++ --version
-cmake -S . -B build -DBUILD_SHARED_LIBS=OFF -DRYML_BUILD_TOOLS=ON
-cmake --build build -j --target ryml-yaml-events
-file=$(find build -name 'ryml-yaml-events*' -type f)
+cmake -S rapidyaml -B rapidyaml/build -DRYML_BUILD_TOOLS=ON -DBUILD_SHARED_LIBS=OFF
+cmake --build rapidyaml/build -j --target ryml-yaml-events
+file=$(find rapidyaml/build -name 'ryml-yaml-events*' -type f)
 cp -fav $file /build/bin/ryml-yaml-events
-cd ..
+
 rm -rf rapidyaml
