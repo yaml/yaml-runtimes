e89251dc4434a1aa257f4166155c61fb17cc4fe6
Joao Paulo Magalhaes <dev@jpmag.me>
2022-02-04 22:54:07 +0000

    Use a debug build for rapidyaml

--- docker/static/utils/cpp-rapidyaml-build.sh
+++ docker/static/utils/cpp-rapidyaml-build.sh
@@ -7,7 +7,7 @@ cd /tmp
 git clone --recursive https://github.com/biojppm/rapidyaml
 
 cmake --version
-cmake -S rapidyaml -B rapidyaml/build -DRYML_BUILD_TOOLS=ON -DBUILD_SHARED_LIBS=OFF
+cmake -S rapidyaml -B rapidyaml/build -DRYML_BUILD_TOOLS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Debug
 cmake --build rapidyaml/build -j --target ryml-yaml-events
 file=$(find rapidyaml/build -name 'ryml-yaml-events*' -type f)
 cp -fav $file /build/bin/ryml-yaml-events
