77de4afc2c97b0cb71da04748722d2fec119a244
Tina Müller <cpan2@tinita.de>
2022-02-26 21:42:43 +0100

    Build from tarfile listed in source

--- docker/static/utils/cpp-rapidyaml-build.sh
+++ docker/static/utils/cpp-rapidyaml-build.sh
@@ -3,12 +3,15 @@
 set -x
 cd /tmp
 
-git clone --recursive --depth 1 --branch v0.4.0 https://github.com/biojppm/rapidyaml
+cp $SOURCE .
+tar xvf rapidyaml-$VERSION-src.tgz
+cd rapidyaml-$VERSION-src
 
 cmake --version
-cmake -S rapidyaml -B rapidyaml/build -DRYML_BUILD_TOOLS=ON -DRYML_WITH_TAB_TOKENS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Debug
-cmake --build rapidyaml/build -j --target ryml-yaml-events
-file=$(find rapidyaml/build -name 'ryml-yaml-events*' -type f)
+cmake -S . -B ./build \
+  -DRYML_BUILD_TOOLS=ON -DRYML_WITH_TAB_TOKENS=ON \
+  -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Debug
+cmake --build ./build -j --target ryml-yaml-events
+file=$(find ./build -name 'ryml-yaml-events*' -type f)
 cp -fav $file /build/bin/ryml-yaml-events
 
-rm -rf rapidyaml
